ccd4de30563bdc78f4259ddc4f37e2dad6463cc1
nicoli silva <nicoli_silva151@estudante.sesisenai.org.br>
2026-06-22 20:13:51 -0300

    q1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿ double[] notas = new double[5];
+        double soma = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Informe a nota: ");
+            notas[i] = double.Parse(Console.ReadLine());
+
+            soma += notas[i];
+        }
+
+        double media = soma / 5;
+
+        Console.WriteLine("Soma das notas: " + soma);
+        Console.WriteLine("Média final: " + media);
+
